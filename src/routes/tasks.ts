51baf7e1566f88dd0ebe2eb3ae95945a40311b1a
Tiago-92 <tiagoamaralcbei@hotmail.com>
2023-06-12 19:17:31 -0300

    feat: finalizado todas rotas do CRUD

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -30,22 +30,71 @@ export async function tasksRoutes(app: FastifyInstance) {
     return task
   })
 
-  /*app.post('/tasks', async (request) => {
+  app.post('/tasks', async (request) => {
     const bodySchema = z.object({
       title: z.string(),
       description: z.string(),
-      date: z.date()
+      date: z.string(),
+      status: z.string()
     })
 
-    const { title, description, date } = bodySchema.parse(request.body)
+    const { title, description, status, date } = bodySchema.parse(request.body)
 
     const task = await prisma.tasks.create({
       data: {
         title,
         description,
+        status,
         date,
-        userR: requ
+        userId: 'f9efb206-3b74-407c-85b7-6a674effe5c5',
+      },
+    })
+
+    return task
+  })
+
+  app.put('/tasks/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.string(),
+      status: z.string()
+    })
+
+    const { title, description, date, status } = bodySchema.parse(request.body)
+
+    const task = await prisma.tasks.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+        date,
+        status
       }
     })
-  }) */
+
+    return task
+  })  
+
+  app.delete('/tasks/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const task = await prisma.tasks.delete({
+      where: {
+        id,
+      }
+    })
+  })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { tasksRoutes } from './routes/tasks'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
 
 app.register(tasksRoutes)
 
